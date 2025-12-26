13771b76b7adda366450d6d6a0c67d4e566d9671
Yanitsharma <yanit.sharma.mec22@itbhu.ac.in>
2025-12-26 19:09:37 +0530

    changed

--- Frontend/src/Components/FriendsPage.jsx
+++ Frontend/src/Components/FriendsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState ,useEffect} from "react";
-const API_BASE_URL = 'http://localhost:2000';
+const API_BASE_URL = 'https://eeexpensetracker.vercel.app/';
 const FriendsPage = ({ navigateHome, groupId }) => {
   const ArrowRightIcon = () => ( <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5 text-green-400"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg> );
 
